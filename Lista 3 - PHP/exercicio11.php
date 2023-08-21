67a80fe5ecbc06aecf91f85deacdf9f9396254dd
Po747o <thayna.albuquerque@estudante.ifro.edu.br>
2023-08-21 17:24:06 -0400

    Tentativas de resolver o exercício 11

--- Lista 3 - PHP/exercicio11.php
+++ Lista 3 - PHP/exercicio11.php
@@ -1,34 +1,20 @@
 <?php
 $n = $_POST["quant"];
-$total = array($n);
+array($n);
 
-for ($i = 0; $i < $n; $i++){
+?>
+
+<html>
+
+<form method="post" action="includeexercicio11.php">
+    <?php
+for ($i = 0; $i < $n; $i++) {
 
     include "includeexercicio11.html";
 
 };
-?>
-//3 4 4 2 1 5 
-<html>
-<input class="botao" type="submit" value="Checar">
+    ?>
+    <input class="botao" type="submit" value="Checar">
+</form>
+
 </html>
-
-<?php
-include "includeexercicio11.html";
-
-for ($n; $n <= 4; $n++) {
-    
-    if ($total[$n] > $total[$n + 1]) {
-
-        $maior = $n;
-
-    }
-    else if ($maior) {
-
-    }
-
-};
-
-echo 'O número ' , $maior , ' é o maior e foi lido ' , $lido , ' vezes.';
-
-?>
