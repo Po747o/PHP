6bde2e0e424102eac6ee3cc4410a9597fddcfcff
Po747o <thayna.albuquerque@estudante.ifro.edu.br>
2023-09-26 11:47:08 -0400

    Tentativas exercício 23

--- Lista3-PHP/exercicio23.php
+++ Lista3-PHP/exercicio23.php
@@ -1,35 +1,40 @@
 <?php
 $aleatorio = rand(1, 10);
 $chute = $_POST["chute"];
-$tentativas;
+$tentativas = 0;
+$exec = 1;
 
-while ($chute != $aleatorio) {
+if (exec)
 
-    if (!is_numeric($chute) || $chute != $aleatorio) {
+
+    if ($chute > 10 | $chute < 1) {
 
         echo 'Você deve fornecer um valor válido, ou seja, apenas números no intervalo de 1 a 1000.' . "</br>";
         echo "\n";
-    }
-
-    if ($chute == $aleatorio) {
-
-        echo 'Eba! Na mosca! Você adivinhou o número ' .$aleatorio. " em " .$tentativas. " tentativas! \n";
-        break;
+        include 'exercicio23include.html';
     }
     else {
 
-        if ($chute < $aleatorio) {
+        if ($chute == $aleatorio) {
 
-            echo 'Não foi dessa vez! Chute um número maior!';
-            $tentativas++;
+            echo 'Eba! Na mosca! Você adivinhou o número ' .$aleatorio. " em " .$tentativas. " tentativas! \n";
         }
         else {
-
-            echo 'Não foi dessa vez! Chute um número menor!';
-            $tentativas++;
-
+    
+            if ($chute < $aleatorio) {
+    
+                echo 'Não foi dessa vez! Chute um número maior!';
+                include 'exercicio23include.html';
+                $tentativas++;
+            }
+            else {
+    
+                echo 'Não foi dessa vez! Chute um número menor!';
+                include 'exercicio23include.html';
+                $tentativas++;
+            }
+            
         }
     }
-    echo "Você tentou " .$tentativas. " vezes.";
-}
+    echo 'Você tentou ' .$tentativas. ' vezes';
 ?>
